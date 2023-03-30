edf07d51a28441978b0cba4d8aa88ebe42389d72
Hun5LEE <jhlee091697@gmail.com>
2023-03-30 23:45:07 +0900

    second commit

--- src/App.tsx
+++ src/App.tsx
@@ -6,6 +6,11 @@ function App(): JSX.Element {
   return (
     <div className="App">
       <Header />
+      <img
+        className="main_bg"
+        src={process.env.PUBLIC_URL + "./Img/main_bg.jpg"}
+        alt=""
+      />
     </div>
   );
 }
--- src/Components/Header.tsx
+++ src/Components/Header.tsx
@@ -1,15 +1,30 @@
 import React from "react";
 import "../ComponentsCss/Header.css";
+import SearchIcon from "@mui/icons-material/Search";
+import ListIcon from "@mui/icons-material/List";
+import AccountCircleIcon from "@mui/icons-material/AccountCircle";
+import ShoppingCartIcon from "@mui/icons-material/ShoppingCart";
 function Header() {
   return (
     <div className="header">
-      <div className="header_logo">
-        <h2>Logo</h2>
+      <ListIcon className="header_left_menu" sx={{ fontSize: 40 }} />
+      <div className="header_center">
+        <div className="header_logo">
+          <img src={process.env.PUBLIC_URL + "./Img/logo2.png"} alt="" />
+        </div>
+        <div className="header_search">
+          <input type="text" />
+          <SearchIcon />
+        </div>
+        <div className="header_rank_product">1 - 애플워치 7</div>
       </div>
-      <div className="header_search">
-        <input type="text" />
+      <div className="header_right_user">
+        <AccountCircleIcon
+          className="header_right_account"
+          sx={{ fontSize: 32 }}
+        />
+        <ShoppingCartIcon sx={{ fontSize: 32 }} />
       </div>
-      <div className="header_menu">Nav</div>
     </div>
   );
 }
